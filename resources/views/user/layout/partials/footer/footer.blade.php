1bc5bffafee1061820108c035b2bf2376bd79431
Teguhriyadi <ilham.teguh55@gmail.com>
2022-12-19 01:07:44 +0700

    Update

--- resources/views/user/layout/partials/footer/footer.blade.php
+++ resources/views/user/layout/partials/footer/footer.blade.php
@@ -1,87 +1,44 @@
+@php
+    use App\Models\Pengaturan\ProfilPerusahaan;
+@endphp
+
 <footer id="footer">
     <div class="footer-top">
         <div class="container">
             <div class="row">
-                <div class="col-lg-3 col-md-6 footer-contact">
+                <div class="col-md-6 footer-contact">
+                    @php
+                        $profil = ProfilPerusahaan::first();
+                    @endphp
                     <h3>Loveable</h3>
                     <p>
-                        Fotografi <br />
-                        adalah seni untk<br />
-                        Mencintai <br /><br />
-                        <strong>Phone:</strong> +1 5589 55488 55<br />
-                        <strong>Email:</strong> info@example.com<br />
+                        {{ $profil->deskripsi_singkat }} <br />
+                        <br>
+                        <strong>No. Telepon:</strong> {{ $profil->nomor_hp }} <br />
+                        <strong>Email:</strong> {{ $profil->email }} <br />
                     </p>
                 </div>
 
-                <div class="col-lg-2 col-md-6 footer-links">
-                    <h4>Portfolio</h4>
-                    <ul>
-                        <li>
-                            <i class="bx bx-chevron-right"></i>
-                            <a href="portfolio.html">Fotografi</a>
-                        </li>
-                        <li>
-                            <i class="bx bx-chevron-right"></i>
-                            <a href="#">Videografi</a>
-                        </li>
-                        <li>
-                            <i class="bx bx-chevron-right"></i>
-                            <a href="#">Editing</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="col-lg-3 col-md-6 footer-links">
-                    <h4>Price List</h4>
-                    <ul>
-                        <li>
-                            <i class="bx bx-chevron-right"></i>
-                            <a href="jasa.html">Fotografi</a>
-                        </li>
-                        <li>
-                            <i class="bx bx-chevron-right"></i>
-                            <a href="#">Videografi</a>
-                        </li>
-                        <li>
-                            <i class="bx bx-chevron-right"></i>
-                            <a href="#">Editing</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="col-lg-4 col-md-6 footer-links">
-                    <h4>Syarat dan Ketentuan</h4>
-                    <a href="timeline.html" class="text-dark">Tamen quem nulla quae legam multos aute sint
-                        culpa legam
-                        noster magna</a>
-                        <h4 class="mt-5">Kebijakan dan privasi</h4>
-                        <a href="kebijakan.html" class="text-dark">Tamen quem nulla quae legam multos aute sint
-                            culpa legam
-                            noster magna</a>
-                        </div>
-                    </div>
+                <div class="col-md-6">
+                    <h3>Deskripsi</h3>
+                    {{ $profil->deskripsi }}
                 </div>
             </div>
-            <div class="container d-md-flex py-4">
-                <div class="me-md-auto text-center text-md-start">
-                    <div class="copyright">
-                        &copy; Copyright
-                        <a href="http://www.loveablegroup.id/" target="_blank">
-                            <strong>
-                                <span>Loveable Publishing</span>
-                            </strong>
-                        </a>. All Rights Reserved
-                    </div>
-                    <div class="credits">
-                        Desain Oleh Sophia Tirova
-                    </div>
-                </div>
-                <div class="social-links text-center text-md-right pt-3 pt-md-0">
-                    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                    <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-                </div>
+        </div>
+    </div>
+    <div class="container d-md-flex py-4">
+        <div class="me-md-auto text-center text-md-start">
+            <div class="copyright">
+                &copy; Copyright
+                <a href="http://www.loveablegroup.id/" target="_blank">
+                    <strong>
+                        <span>Loveable Publishing</span>
+                    </strong>
+                </a>. All Rights Reserved
             </div>
-        </footer>
+            <div class="credits">
+                Desain Oleh Sophia Tirova
+            </div>
+        </div>
+    </div>
+</footer>
